b7e6f2ae0391420986b16a245b83a9a1845c4e17
asi-skumar <sandeep.kumar@pepcus.com>
2020-01-12 22:18:41 +0530

    Modified spring auth version.

--- src/main/java/sandeep/demo/spring/security/AuthorizationServerConfiguration.java
+++ src/main/java/sandeep/demo/spring/security/AuthorizationServerConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
